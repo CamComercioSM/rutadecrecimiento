dcbd999813982db38aeda022efa09362c4859ebf
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2024-08-09 17:05:54 -0500

    cambio de fontawesome
    se actualizó la libreria remota (cdn)
    https://kit.fontawesome.com/440d840df1.js  que estaba asociada a cerobox, y se colocó
    https://kit.fontawesome.com/01ae7d183b.js  asociuada a la cuenta jpllinas@ccsm.org.co

--- public_html/resources/views/website/layouts/main_dashboard.blade.php
+++ public_html/resources/views/website/layouts/main_dashboard.blade.php
@@ -18,7 +18,7 @@
         
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://kit.fontawesome.com/440d840df1.js" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/01ae7d183b.js" crossorigin="anonymous"></script> 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         
 </head>
