b42d3f5079f75ea1117068e6ef556132b420611e
cpc591 <39139505+cpc591@users.noreply.github.com>
2025-02-03 11:45:11 -0500

    ajustes para la parte pública

--- public_html/vendor/digital-creative/collapsible-resource-manager/resources/js/tool.js
+++
@@ -1,5 +0,0 @@
-import CollapsibleResourceManager from './components/CollapsibleResourceManager'
-
-Nova.booting((Vue, router, store) => {
-    Vue.component('collapsible-resource-manager', CollapsibleResourceManager)
-})
