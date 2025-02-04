c3be15005e02d6a9f2f908ab7bad24eb44c8b847
cpc591 <39139505+cpc591@users.noreply.github.com>
2025-02-04 11:14:41 -0500

    ajustes de scroll en el menu

--- resources/views/website/layouts/header_company.blade.php
+++ resources/views/website/layouts/header_company.blade.php
@@ -1,4 +1,4 @@
-<header data-step="1" data-title="Bienvenido a Ruta C" data-intro="En este menú encontrará las diferentes opciones de nuestra plataforma de clientes.">
+<header data-step="1" data-title="Bienvenido a Ruta C" data-intro="En este menú encontrará las diferentes opciones de nuestra plataforma de clientes." style=" overflow-y: auto;">
     <ul>
         <li>
             <a class="logo" href="{{route('company.dashboard')}}">
