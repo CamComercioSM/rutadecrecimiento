678e30d992b7c8b1ff0275e1458c266dede4061b
ingllinasramirez <ing.llinasramirez@gmail.com>
2025-02-21 00:37:52 -0500

    Update DiagnosticoController.php

--- app/Http/Controllers/DiagnosticoController.php
+++ app/Http/Controllers/DiagnosticoController.php
@@ -20,41 +20,37 @@ class DiagnosticoController extends Controller
     {
         $unidadProductiva = UnidadProductivaService::getUnidadProductiva();
 
-        if( !($unidadProductiva->sector_id > 0) ){
+        if (!($unidadProductiva->sector_id > 0)) {
             return redirect()->route('company.complete_info');
         }
 
-        if($request->anual_sales != null)
-        {
+        if ($request->anual_sales != null) {
             $unidadProductiva->anual_sales = $request->anual_sales;
             $unidadProductiva->save();
         }
-        
+
         $data = [
-            'footer'=> CommonService::footer(),
-            'links'=> CommonService::links(),
-            'company'=> $unidadProductiva,
-            'arranquePOR'=> UnidadProductivaService::validarDiagnostico($unidadProductiva),
+            'footer' => CommonService::footer(),
+            'links' => CommonService::links(),
+            'company' => $unidadProductiva,
+            'arranquePOR' => UnidadProductivaService::validarDiagnostico($unidadProductiva),
         ];
 
-        if($unidadProductiva->anual_sales != null)
-        {
-            $diagnostico = Diagnostico::where([ 
-                ['diagnostico_etapa_id', $unidadProductiva->etapa_id], 
-                ['diagnostico_conventas', $unidadProductiva->anual_sales] 
+        if ($unidadProductiva->anual_sales != null) {
+            $diagnostico = Diagnostico::where([
+                ['diagnostico_etapa_id', $unidadProductiva->etapa_id],
+                ['diagnostico_conventas', $unidadProductiva->anual_sales]
             ])->first();
-            
-            if($diagnostico == null)
-            {
-                $diagnostico = Diagnostico::where([ 
-                    ['diagnostico_etapa_id', null], 
-                    ['diagnostico_conventas', $unidadProductiva->anual_sales == 1] 
-                ])->first();
-            }   
 
-            if($diagnostico->diagnostico_conventas)
-            {
-                $data['preguntas'] = 
+            if ($diagnostico == null) {
+                $diagnostico = Diagnostico::where([
+                    ['diagnostico_etapa_id', null],
+                    ['diagnostico_conventas', $unidadProductiva->anual_sales == 1]
+                ])->first();
+            }
+
+            if ($diagnostico->diagnostico_conventas) {
+                $data['preguntas'] =
                     DiagnosticoPregunta::where('pregunta_rango_ventas', 1)
                     ->union(
                         DiagnosticoPregunta::whereNull('diagnostico_id')->where('pregunta_rango_ventas', '!=', 1)
@@ -63,8 +59,7 @@ class DiagnosticoController extends Controller
                         DiagnosticoPregunta::where('diagnostico_id', $diagnostico->diagnostico_id)
                     )
                     ->get();
-            }
-            else{
+            } else {
                 $data['preguntas'] = DiagnosticoPregunta::whereNull('diagnostico_id')->where('pregunta_rango_ventas', '!=', 1)
                     ->union(
                         DiagnosticoPregunta::where('diagnostico_id', $diagnostico->diagnostico_id)
@@ -74,10 +69,9 @@ class DiagnosticoController extends Controller
 
             $data['preguntas']->load('opciones');
 
-            if($diagnostico->diagnostico_conventas)
-            {
+            if ($diagnostico->diagnostico_conventas) {
                 $sector = $unidadProductiva->sector()->first();
-                $opciones = VentasAnuales::where('sectorCODIGO',  $sector->sectorCODIGO )->get();
+                $opciones = VentasAnuales::where('sectorCODIGO',  $sector->sectorCODIGO)->get();
 
                 if (isset($data['preguntas'][0]) && isset($data['preguntas'][0]->opciones)) {
                     $data['preguntas'][0]->setRelation('opciones', collect([]));
@@ -87,12 +81,12 @@ class DiagnosticoController extends Controller
                     $item = new PreguntaOpcion();
                     $item->opcion_id = $opcion->VentasAnualesOpcionID;
                     $item->opcion_variable_response = $opcion->ventasAnualesNOMBRE;
-                    
+
                     $data['preguntas'][0]->opciones->push($item);
                 }
             }
 
-            $data['diagnosticoId'] = $diagnostico->diagnostico_id;         
+            $data['diagnosticoId'] = $diagnostico->diagnostico_id;
         }
 
         return view('website.company.diagnostic', $data);
@@ -102,7 +96,7 @@ class DiagnosticoController extends Controller
     {
         $unidadProductiva = UnidadProductivaService::getUnidadProductiva();
 
-        $diagnostico = New DiagnosticoResultado();
+        $diagnostico = new DiagnosticoResultado();
         $diagnostico->unidadproductiva_id = $unidadProductiva->unidadproductiva_id;
         $diagnostico->resultado_puntaje = 0;
         $diagnostico->save();
@@ -112,62 +106,51 @@ class DiagnosticoController extends Controller
         $suma_grupos = [];
         $ventaanual_id = 1;
 
-        foreach ($request->all() as $key => $value) 
-        {
-            if( str_starts_with($key, 'variable-') )
-            {
+        foreach ($request->all() as $key => $value) {
+            if (str_starts_with($key, 'variable-')) {
                 $pregunta_id = str_replace('variable-', '', $key);
 
                 $pregunta = DiagnosticoPregunta::find($pregunta_id);
                 $opcion = PreguntaOpcion::find($value);
-                if($pregunta->pregunta_rango_ventas){
-                    $ventaanual_id = $opcion->opcion_id;
-                }
 
-                $respuesta = New DiagnosticoRespuesta();
+                $respuesta = new DiagnosticoRespuesta();
                 $respuesta->resultado_id = $diagnostico->resultado_id;
                 $respuesta->pregunta_id = $pregunta->pregunta_id;
                 $respuesta->diagnosticorespuesta_valor = $opcion->opcion_variable_response;
-
-                $respuesta->diagnosticorespuesta_porcentaje = 
-                    ($pregunta->pregunta_porcentaje/100) * ($opcion->opcion_percentage);
-
+                $respuesta->diagnosticorespuesta_porcentaje = $opcion->opcion_percentage;
                 $respuesta->save();
 
-                if( !isset($suma_grupos[$pregunta->preguntagrupo_id]) )
-                {
+                if (!isset($suma_grupos[$pregunta->preguntagrupo_id])) {
                     $suma_grupos[$pregunta->preguntagrupo_id] = 0;
                 }
+                $suma_grupos[$pregunta->preguntagrupo_id] += ($pregunta->pregunta_porcentaje / 100) * ($opcion->opcion_percentage);
 
-                $suma_grupos[$pregunta->preguntagrupo_id] += $respuesta->diagnosticorespuesta_porcentaje;
+                if ($pregunta->pregunta_rango_ventas) {
+                    $ventaanual_id = $opcion->opcion_id;
+                }
             }
         }
 
-        $resultado_puntaje = 0;
-        
-        foreach ($grupos as $grupo) 
-        {
-            $resultado_puntaje += $suma_grupos[$grupo->preguntagrupo_id] * ($grupo->preguntagrupo_peso/100);
+        if ($unidadProductiva->anual_sales == 1) {
+            $unidadProductiva->ventaanual_id = $ventaanual_id;
         }
 
-        if ($unidadProductiva->anual_sales == 1) 
-        {
-            $unidadProductiva->ventaanual_id = $ventaanual_id;
-            $unidadProductiva->etapa_id = UnidadProductivaService::getEtapa($resultado_puntaje);
-        } 
-
-
+        $resultado_puntaje = 0;
+        foreach ($grupos as $grupo) {
+            $resultado_puntaje += $suma_grupos[$grupo->preguntagrupo_id] * ($grupo->preguntagrupo_peso / 100);
+        }
+        
+        $unidadProductiva->etapa_id = UnidadProductivaService::getEtapa($resultado_puntaje);
         $diagnostico->resultado_puntaje = $resultado_puntaje;
         $diagnostico->etapa_id = $unidadProductiva->etapa_id;
         $diagnostico->save();
 
         $unidadProductiva->complete_diagnostic = 1;
 
-        
+
         dd($unidadProductiva, $diagnostico);
         $unidadProductiva->save();
 
         return redirect()->route('company.dashboard');
     }
-    
 }
