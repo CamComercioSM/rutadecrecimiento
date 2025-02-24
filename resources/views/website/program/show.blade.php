7ebfce625ae75597825e3af230e7452fa917ea99
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-02-24 11:21:50 -0500

    mostando ls fechas de la convocatoria.

--- resources/views/website/program/show.blade.php
+++ resources/views/website/program/show.blade.php
@@ -17,7 +17,7 @@
                             {{$convocatoria->nombre_convocatoria}}
                         </h2>
                         <h4 class="mt-3">
-                            Desde {{$convocatoria->fecha_apertura_convocatoria}} hasta  {{$convocatoria->fecha_cierre_convocatoria}} 
+                            Desde {{ \Carbon\Carbon::parse($convocatoria->fecha_apertura_convocatoria)->translatedFormat('j \d\e F \d\e Y') }} hasta  {{ \Carbon\Carbon::parse($convocatoria->fecha_cierre_convocatoria)->translatedFormat('j \d\e F \d\e Y') }}
                         </h4>
                         
                         @if($already_subscribed)
