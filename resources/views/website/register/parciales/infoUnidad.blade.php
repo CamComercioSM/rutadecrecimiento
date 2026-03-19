57d9e4a36ac209ed9b388c70ae1ed855d1f6f6e4
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2026-03-19 09:23:24 -0500

    ...

--- resources/views/website/register/parciales/infoUnidad.blade.php
+++ resources/views/website/register/parciales/infoUnidad.blade.php
@@ -81,7 +81,7 @@
 
             <div class="col-12 col-md-12 form-group mb-3">
                 <label class="form-label">Dirección</label>
-                <input type="text" class="form-control" name="address" id="address" placeholder="Dirección"
+                <input type="text" class="form-control " name="address" id="address" placeholder="Dirección"
                     required />
             </div>
         </div>
