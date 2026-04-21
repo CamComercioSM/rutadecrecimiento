8cc5afc7542d88b38be5fd323544db698da9a7d9
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2026-04-21 11:27:29 -0500

    ......

--- resources/views/website/layouts/helper.blade.php
+++ resources/views/website/layouts/helper.blade.php
@@ -1,70 +1,21 @@
-<div class="c-helper" data-step="9" data-intro="Nuestro ayudante te dara recomendaciones y consejos">
-    @if(count($helper_notifications) > 0)
-    <ul class="from-admin">
-        @foreach($helper_notifications as $notification)
-        <li>
-            @if($notification->url != null)
-            <a class="with-link" href="{{$notification->url}}" target="_blank">
-                <h6>{{$notification->titulo}}</h6>
-                <p>{{$notification->descripcion}}</p>
-            </a>
-            @else
-            <a>
-                <h6>{{$notification->titulo}}</h6>
-                <p>{{$notification->descripcion}}</p>
-            </a>
-            @endif
-        </li>
-        @endforeach
-    </ul>
-    @endif
-    <button><span>C</span></button>
-</div>
 
-@section('js')
-<script>
-    $(document).ready(function () {
-            $('header .dashboard').addClass('active');
+    <div id="widget-whatsapp-raiz"></div>
+    <script>
+        window.CONFIGURACION_WIDGET_WHATSAPP = {
+            telefono: "573218150243",
+            posicion: "derecha-abajo", // ver lista de posiciones más abajo
+            textoBoton: "",
+            mensajePredeterminado: "Hola, necesito más informacion de.....",
+            nombreMarca: "RutaC CamComercoSM",
+            subtituloMarca: "Asesor Ruta C",
+            textoBienvenida: "¡Hola!, {{ $company->business_name }}, Cuéntanos en qué podemos apoyarte 😊",
+            abrirAutomaticamente: false,            
+            radioBorde: "60px",
+            margenInferior: "30px",
+            margenLateral: "30px",
+            // cualquier campo que omitas usa el valor por defecto del widget
+        };
+    </script>
+    <script src="https://cdnsicam.net/js/whatsapp-widget.js"></script>
 
-            var notifications = $('.c-helper .from-admin li');
-            var length = notifications.length;
-            var fechaInicio = new Date(localStorage.getItem('alertas')).getTime();
-            var fechaFin = new Date().getTime();
-            var diff = fechaFin - fechaInicio;
-            console.log(diff);
-            if (diff > 86400000 ) {
-                    if (length > 0) {
-                            $(notifications).each(function (i) {
-                                    var $li = $(this);
-                                    setTimeout(function () {
-                                            if (i > 0) {
-                                                    //Ocultamos el LI anterior
-                                                    $(".c-helper ul li:nth-child(" + i + ")").find('a').slideToggle('fast');
-                                            }
 
-                                            // Si es la ultima notificacion se ejecuta una funcion diferente
-                                            i === (length - 1) ? lastNotification($li) : nextNotification($li);
-
-                                    }, 8000 * (i + 1));
-
-                                    localStorage.setItem('alertas', new Date());
-                            });
-
-                            function nextNotification($li) {
-                                    $li.find('a').slideToggle('fast');
-                            }
-
-                            function lastNotification($li) {
-                                    $li.find('a').slideToggle('fast').delay(3000).slideToggle('fast');
-                            }
-                    } else {
-                            // Si no tienen nada agregado desde el panel administrativo, al presionar muestra el consejo por defecto
-                            //click c-helper button
-                            $('.c-helper button').click(function () {
-                                    $('.c-helper a').slideToggle();
-                            });
-                    }
-            }
-    });
-</script>
-@endsection
